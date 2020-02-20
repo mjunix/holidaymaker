1cb02296441d9725f9c083d66d9db5e3e1c0dc9d
Johan Mattsson <johan.mattsson@hotmail.se>
2020-02-20 22:30:01 +0100

    Implement showMainMenu()

--- src/com/company/Holidaymaker.java
+++ src/com/company/Holidaymaker.java
@@ -1,13 +1,47 @@
 package com.company;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Holidaymaker {
     ResultSet resultSet;
     PreparedStatement statement;
     Connection conn;
+    private Scanner scanner = new Scanner(System.in);
 
     public Holidaymaker() throws SQLException {
         conn = DriverManager.getConnection("jdbc:mysql://localhost/holidaymaker?user=root&password=toor&serverTimezone=UTC");
+        showMainMenu();
+    }
+
+    private void showMainMenu() {
+        while(true) {
+            System.out.println("\nMain menu:");
+            System.out.println("1. Register customer");
+            System.out.println("2. Make reservation");
+            System.out.println("0. Exit");
+            System.out.print("Choice: ");
+
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+                case 1:
+                    registerCustomer();
+                    break;
+                case 2:
+                    makeReservation();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Unknown choice, try again!");
+            }
+        }
+    }
+
+    private void makeReservation() {
+    }
+
+    private void registerCustomer() {
     }
 }
