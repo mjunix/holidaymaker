2d2ccc8b5b08eb70f322060638ecf39a968063dd
Johan Mattsson <johan.mattsson@hotmail.se>
2020-02-20 21:55:56 +0100

    Connect to database

--- src/com/company/Holidaymaker.java
+++ src/com/company/Holidaymaker.java
@@ -1,4 +1,13 @@
 package com.company;
 
+import java.sql.*;
+
 public class Holidaymaker {
+    ResultSet resultSet;
+    PreparedStatement statement;
+    Connection conn;
+
+    public Holidaymaker() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/holidaymaker?user=root&password=toor&serverTimezone=UTC");
+    }
 }
